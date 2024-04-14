333964cb9f0ebd8ee283bc58a55e240e7003ccd4
achmadshobari <67608822+achmadshobari@users.noreply.github.com>
2024-04-15 04:35:58 +0700

    Delete auth directory

--- auth/auth.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtConstants = void 0;
-exports.jwtConstants = {
-    secret: 'do not use this value',
-};
-//# sourceMappingURL=auth.constants.js.map
