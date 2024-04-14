a436c66e5ce97cdeeaca9f49eb93bc185d3b5df7
achmadshobari <67608822+achmadshobari@users.noreply.github.com>
2024-04-15 04:37:21 +0700

    Delete users directory

--- users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from 'src/entity/user.entity';
-export declare class UsersService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    getUserByEmail(email: string): Promise<User | null>;
-    createUser(userData: Partial<User>): Promise<User>;
-}
